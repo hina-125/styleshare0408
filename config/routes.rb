8785438a14405225426e963a1ed1bb4721490cf9
hina.k <nahhi125@i.softbank.jp>
2021-03-31 13:46:36 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # root to: 'posts#index'を下記に変更
   root to: 'toppages#index'
+  
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
