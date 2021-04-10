8563b6159bcc23b88e3e773562e6aafa87b25ab8
hina.k <nahhi125@i.softbank.jp>
2021-04-10 11:28:29 +0000

    for heroku5

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   #process resize_to_fill: [270, 360]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
