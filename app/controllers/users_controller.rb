d1d72127fe227b60a6b93d9ab8cbb3fa98aea45b
hina.k <nahhi125@i.softbank.jp>
2021-04-11 07:58:30 +0000

    usersshow2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :forbid_login_user, only: [:index]
   
   def index
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @posts = @user.posts.order(id: :desc).page(params[:page]).per(5)
+    
+    if @user == current_user
+    else
+    redirect_to root_url
+    end
   end
 
   def new
@@ -29,5 +35,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+  
+  def fobid_login_user
+    if @current_user
+      flash[:notice]="ログイン中です"
+      redirect_to root_url
+    end
   end
 end
